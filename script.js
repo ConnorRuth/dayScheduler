01d37df0936d81591e30e9bbfb761b0cc5697b51
ConnorRuth <152222829+ConnorRuth@users.noreply.github.com>
2024-01-22 18:16:18 -0800

    added clear button functionality

--- script.js
+++ script.js
@@ -1,50 +1,38 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   var today = dayjs();
   var currentHour = today.format('H');
-  var textFields = [$('#textarea1'),$('#textarea2'),$('#textarea3')]
-  var colorBoxes = [$('#9'),$('#10'),$('#11'),$('#12'),$('#13'),$('#14'),$('#15'),$('#16'),$('#17')]
+  // convert string to number as it was occasionally not working this seemed to fix
+  currentHour = +currentHour;
+  var textFields = [$('#textarea1'),$('#textarea2'),$('#textarea3'),$('#textarea4'),$('#textarea5'),$('#textarea6'),$('#textarea7'),$('#textarea8'),$('#textarea9')]
+  var colorBoxes = [$('#hour-9'),$('#hour-10'),$('#hour-11'),$('#hour-12'),$('#hour-13'),$('#hour-14'),$('#hour-15'),$('#hour-16'),$('#hour-17')]
+ // saving the text fields to local storage
   $('.saveBtn').on('click', function(){
     var i = $('.saveBtn').index(this);
     localStorage.setItem("textFields" + i, textFields[i].val());
-    }
-  )
-  $(function () {
+    })
+    //get the saved text fields upon loading
+    $(function () {
     for(i=0; i<textFields.length; i++){
-    var savedText = localStorage.getItem("textFields"+i);
-    textFields[i].text(savedText);
-    }})
-
+      var savedText = localStorage.getItem("textFields"+i);
+      textFields[i].text(savedText);}
+    });
+    //take the current hour and compare it to the hour represented by each item in colorBoxes array and adjust their classes accordingly
 function colorToHour(){
   for(j=0;j<colorBoxes.length;j++){
-    if (+currentHour === j+9) {
+    if (currentHour === j+9) {
         $(colorBoxes[j]).removeClass("past", "future").addClass("present");} 
-    if(+currentHour < j+9){
+    if(currentHour < j+9){
         $(colorBoxes[j]).removeClass("past", "present").addClass("future");} 
-    if(+currentHour > j+9){
+    if(currentHour > j+9){
         $(colorBoxes[j]).removeClass("future", "present").addClass("past");}
     }}
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  $(function countdown() {
+    //run each of these so that they load immidaitely upon openning window updater function adds a sec or 2 delay to start
+$(function init(){
+  colorToHour();
+  $('#currentDay').text(today.format('MMMM D, YYYY hh:mm A'));
+});
+ // run at timer to update our colors and time at the top checking every 2 seonds as to prevent less lag but keep it within close margin of proper time
+  $(function updater() {
     var timer = 0;
       setInterval(function() {
         if(timer === 0 || timer%2 === 0){
@@ -55,8 +43,10 @@ function colorToHour(){
         colorToHour();
     }, 1000);
 });
-console.log(currentHour);
-console.log(typeof +currentHour);
-console.log(colorBoxes[3]);
-
+//clear storage and current events in the day planner
+$('#purgeBtn').on('click', function(){
+  for(k=0; k<textFields.length; k++){
+    localStorage.clear("textFields" + k);
+    $(textFields[k]).val("");}
+  });
 });
